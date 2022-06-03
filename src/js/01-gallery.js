241481e9478468872a8960d2f83ab6e998be5938
shuro4ka <zzlesia77@gmail.com>
2022-06-03 20:36:44 +0100

    add gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,13 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+function createGallery(numberOfItems) {
+    const items = [];
+    for(let i = 0; i < numberOfItems; i++) {
+        const item = document.createElement("listItem");
+        items.push(item);
+    }
+    gallery.append(...items);
+}
+console.log(items);
