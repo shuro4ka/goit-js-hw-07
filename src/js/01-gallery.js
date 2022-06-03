b5743fe4e7a6bb3b1d3bbf5621e7b8e78dd00ef1
shuro4ka <zzlesia77@gmail.com>
2022-06-03 22:34:40 +0100

    add markup

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,13 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const gallery = document.querySelector(".gallery");
 
-function createGallery(numberOfItems) {
-    const items = [];
-    for(let i = 0; i < numberOfItems; i++) {
-        const item = document.createElement("listItem");
-        items.push(item);
-    }
-    gallery.append(...items);
+const galleryContainer = document.querySelector('.gallery');
+const imagesMarkup = createGalleryMurkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+galleryContainer.addEventListener('click', onImageClick);
+
+console.log(createGalleryMurkup(galleryItems));
+
+function createGalleryMurkup (galleryItems){
+return galleryItems
+    .map(({preview, description, original}) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+    </div>`
+    })
+    .join('');
+
 }
-console.log(items);
+
+function onImageClick(event){
+    event.preventDefault();
+    if(event.target.nodeMode !== "IMG") {
+        return;
+    }
+    console.log(event.target);
+}
